8a1a9bbdd5e7f5a0d2e94bd8330c2a4c15ee2196
Furkan <kvkfurkan@hotmail.com>
2024-08-30 21:32:26 +0300

    fragment anger design added.

--- app/src/main/java/dev/furkankavak/insideout/fragments/GivingFragment.kt
+++ app/src/main/java/dev/furkankavak/insideout/fragments/GivingFragment.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.bumptech.glide.Glide
-import dev.furkankavak.insideout.R
 import dev.furkankavak.insideout.databinding.FragmentGivingBinding
 
 
@@ -27,12 +24,7 @@ class GivingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val imageView = view.findViewById<ImageView>(R.id.gifImageView)
-        val gifUrl = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDc1MGQxaWRtc3IycWRsc3BuMWV4bXAxZGVyMjhmamx2OWFjanhiOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11tTNkNy1SdXGg/giphy.webp"
 
-        Glide.with(this)
-            .load(gifUrl)
-            .into(imageView)
 
 
     }
--- app/src/main/java/dev/furkankavak/insideout/fragments/OptimismFragment.kt
+++ app/src/main/java/dev/furkankavak/insideout/fragments/OptimismFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.furkankavak.insideout.R
+import com.bumptech.glide.Glide
+import dev.furkankavak.insideout.databinding.FragmentOptimismBinding
 
 class OptimismFragment : Fragment() {
+
+    private var _binding: FragmentOptimismBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_optimism, container, false)
+        _binding = FragmentOptimismBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageView = binding.gifImageView
+        val gifUrl = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExcDc1MGQxaWRtc3IycWRsc3BuMWV4bXAxZGVyMjhmamx2OWFjanhiOSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/11tTNkNy1SdXGg/giphy.webp"
+
+        Glide.with(this)
+            .load(gifUrl)
+            .into(imageView)
     }
 }
