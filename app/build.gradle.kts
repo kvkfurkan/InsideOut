027d8a2811f10ca11c4a7f847740160f1fa28132
Furkan <kvkfurkan@hotmail.com>
2024-08-30 21:25:55 +0300

    glide library added.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation (libs.glide)
+    annotationProcessor(libs.compiler)
 
 }
